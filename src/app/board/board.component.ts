349aad08affa4113010c9c8914e2a718bfe4fd3c
Dragos Sabau <sabau.dragos@gmail.com>
2017-12-31 12:11:11 +0200

    added add new user story button

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -17,13 +17,13 @@ export class BoardComponent implements OnInit {
   userList = ['Dragos', 'David', 'Bogdan', 'Johny'];
 
   userStoryList = [
-    new UserStory('Create Data Layer for User', 'Dragos', 2, 2, 'story',
+    new UserStory('Create Data Layer for User', 'Dragos', 2, 2,
       'Entities need to be created along with the methods for CRUD operations', 'New'),
-    new UserStory('Create Business Layer for User', 'Johny', 2, 2, 'story',
+    new UserStory('Create Business Layer for User', 'Johny', 2, 2,
       'Create DTOs, business methods for CRUD operations', 'In Progress'),
-    new UserStory('Create View Layer for User', 'Johny', 2, 2, 'story',
+    new UserStory('Create View Layer for User', 'Johny', 2, 2,
       'Create DTOs, business methods for CRUD operations', 'In Review'),
-    new UserStory('Create Infrastructure Layer for User', 'Johny', 2, 2, 'story',
+    new UserStory('Create Infrastructure Layer for User', 'Johny', 2, 2,
       'Create DTOs, business methods for CRUD operations', 'Done')];
 
   constructor(public dialog: MatDialog) {
@@ -53,17 +53,43 @@ export class BoardComponent implements OnInit {
       // show dummy, predefined data
     } else {
       // user wants to open an existing story
+      const itemClone = this.cloneUserStory(item);
       const dialogRef = this.dialog.open(BoardItemDialogComponent, {
         width: '80%',
         height: '60%',
-        data: item
+        data: {itemClone}
       });
 
       dialogRef.afterClosed().subscribe(result => {
         if (result != null) {
-          console.log('The dialog was closed. Status: ' + result.status + ' owner: ' + result.owner);
+          this.copyUserStory(item, result);
+          console.log('Result: Status: ' + result.status
+            + ' owner: ' + result.owner
+            + ' priority: ' + result.priority
+            + ' estimation: ' + result.estimation);
+
+          console.log('Item: Status: ' + result.status
+            + ' owner: ' + result.owner
+            + ' priority: ' + result.priority
+            + ' estimation: ' + result.estimation);
         }
       });
     }
+  }
+
+  cloneUserStory(userStory: UserStory): UserStory {
+    const copy: UserStory = new UserStory(userStory.title, userStory.owner, userStory.priority,
+      userStory.estimation, userStory.description, userStory.status);
+
+    return copy;
+  }
+
+  copyUserStory(item: UserStory, clone: UserStory): void {
+    item.status = clone.status;
+    item.description = clone.description;
+    item.estimation = clone.estimation;
+    item.priority = clone.priority;
+    item.title = clone.title;
+    item.owner = clone.owner;
   }
 }
--- src/app/board/dto/user-story.ts
+++ src/app/board/dto/user-story.ts
@@ -5,21 +5,17 @@ export class UserStory{
   status: string;
   priority: number;
   estimation: number;
-  type: string;
-
 
   constructor(title: string,
               owner: string,
               priority: number,
               estimation: number,
-              type: string,
               description: string,
               status: string) {
     this.title = title;
     this.owner = owner;
     this.priority = priority;
     this.estimation = estimation;
-    this.type = type;
     this.description = description;
     this.status = status;
   }
